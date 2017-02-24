16bd93cd9b0f78d1198c35b0fa82a38a966f68c2
mainframe98 <k.s.werf@hotmail.com>
2017-02-24 12:11:09 +0100

    Make proper use of the unblockable right
    This commit rewrites the internals to make use of the
    unblockable right. This replaces the check for the staff group.
    To ensure maximum compatiblity with the previous versions, two
    new global variables are introduced:
    wgStaffPowersStewardGroupName with the default 'steward'
    and wgStaffPowersShoutWikiMessages with the default true.
    wgStaffPowersStewardGroupName contains the groupname of the
    steward group, a group only blockable by those who have the
    unblockable right. If this global is set to false, NULL or an
    empty string, it will be ignored.
    wgStaffPowersShoutWikiMessages toggles the ShoutWiki specific
    messages when blocking either stewards or those with the
    unblockable right. Its functionality has also been fixed, as it
    previously didn't work. In addition to these changes, the Dutch
    translation has also been updated.
    Edit: Fixed naming, per PHP naming conventions
    Edit 2: Applied fixes advised by Jack Phoenix. I also forgot to
    mention that the this patchset also introduces the shorthand
    array syntax.
    
    Change-Id: I1fccec3b74dd9b0b98994b81e3a3426efca898c0

--- StaffPowers.class.php
+++ StaffPowers.class.php
@@ -5,10 +5,11 @@
  *
  * @file
  * @ingroup Extensions
- * @version 1.3
- * @date 20 November 2015
+ * @version 1.4
+ * @date 23 February 2017
  * @author Łukasz Garczewski <tor@wikia-inc.com>
  * @author Jack Phoenix <jack@countervandalism.net>
+ * @author Mainframe98 <mainframe98@outlook.com>
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 3.0 or later
  * @link https://www.mediawiki.org/wiki/Extension:StaffPowers Documentation
  */
@@ -16,52 +17,59 @@
 class StaffPowers {
 
 	/**
-	 * Makes staff members completely unblockable and stewards unblockable by
-	 * non-staff users.
+	 * Makes users with the unblockable right (staff members) completely unblockable and stewards
+	 * unblockable by non-staff users.
 	 *
 	 * @param Block $block The Block object about to be saved
 	 * @param User $user The user _doing_ the block (not the one being blocked)
 	 * @param array $reason Custom reason as to why blocking isn't possible
 	 * @return bool
 	 */
-	public static function makeUnblockable( $block, $user, $reason ) {
+	public static function makeUnblockable( &$block, &$user, &$reason ) {
+		global $wgStaffPowersStewardGroupName, $wgStaffPowersShoutWikiMessages;
 		$blockedUser = User::newFromName( $block->getRedactedName() );
 
-		if ( empty( $blockedUser ) ) {
+		if ( empty( $blockedUser ) || User::isIP( $blockedUser ) ) {
 			return true;
 		}
 
-		if ( User::isIP( $blockedUser ) ) {
-			return true;
+		$userIsSteward = false;
+		if ( !empty( $wgStaffPowersStewardGroupName ) ) {
+			$userIsSteward = in_array( $wgStaffPowersStewardGroupName, $blockedUser->getEffectiveGroups() );
 		}
 
-		$userIsSteward = in_array( 'steward', $blockedUser->getEffectiveGroups() );
-		if ( !$blockedUser->isAllowed( 'unblockable' ) && !$userIsSteward ) {
+		$userIsUnblockable = $blockedUser->isAllowed( 'unblockable' );
+		if ( !$userIsUnblockable && !$userIsSteward ) {
 			return true;
 		}
 
 		// This exists for interoperability purposes with Wikia's StaffLog extension
-		Hooks::run( 'BlockIpStaffPowersCancel', array( $block, $user ) );
+		Hooks::run( 'BlockIpStaffPowersCancel', [ $block, $user ] );
 
 		// Display a custom reason as to why blocking the specified user isn't
 		// possible instead of the totally unhelpful, default core message
-		$userIsStaff = in_array( 'staff', $blockedUser->getEffectiveGroups() );
-		$blockerIsStaff = in_array( 'staff', $user->getEffectiveGroups() );
 
-		// Don't allow staff to be blocked in any circumstances
-		if ( $userIsStaff ) {
-			$reason = array( 'staffpowers-ipblock-abort' );
-		} elseif ( $userIsSteward && !$blockerIsStaff ) {
-			// and also don't allow stewards to be blocked by non-staff, as per IRC
-			// discussion on 19 January 2014
-			$reason = array( 'staffpowers-steward-block-abort' );
-		} elseif ( $userIsSteward && $blockerIsStaff ) {
-			// This is a possible scenario - staff are allowed to block stewards.
-			// We need to address this situation 'cause this function returns false
-			// by default, so w/o this elseif loop, staff trying to block a steward
-			// will bump into the default core hookaborted message and the block
-			// will fail.
-			return true;
+		// Don't allow users with the unblockable right (staff) to be blocked in any circumstances
+		if ( $userIsUnblockable ) {
+			$reason = [ 'staffpowers-ipblock-abort' ];
+		} elseif ( $userIsSteward ) {
+			if ( $user->isAllowed( 'unblockable' ) ) {
+				// This is a possible scenario - staff are allowed to block stewards.
+				// We need to address this situation 'cause this function returns false
+				// by default, so w/o this elseif loop, staff trying to block a steward
+				// will bump into the default core hookaborted message and the block
+				// will fail.
+				return true;
+			} else {
+				// and also don't allow stewards to be blocked by non-staff, as per IRC
+				// discussion on 19 January 2014
+				$reason = [ 'staffpowers-steward-block-abort' ];
+			}
+		}
+
+		//Override the reason message when not run on ShoutWiki
+		if ( !$wgStaffPowersShoutWikiMessages ) {
+			$reason = [ 'staffpowers-unblockable-abort' ];
 		}
 
 		return false;
